996767e7e78f657e5984888fc561c377f8aa08c7
Andrey Popp <8mayday@gmail.com>
2010-07-11 01:13:22 +0400

    Added build utilities.

---
+++ statics/build.py
@@ -0,0 +1,69 @@
+""" Build."""
+
+from os import mkdir
+from os.path import join
+from shutil import copy
+
+from statics.tree import TreeView
+from statics.configuration import query_script
+from statics.element import ContentElement
+from statics.element import Element
+from statics.element import BinaryElement
+
+__all__ = ["build", "layout"]
+
+
+def ordered_locations(locations):
+    return sorted(locations, reverse=True)
+
+
+def build(site, root_item, locations=None):
+    """ Build `site`."""
+    if locations is None:
+        locations = site.locations
+    if not "/" in [l for l, c, s in locations]:
+        raise ValueError("No script found for root (/) location.")
+
+    builded = []
+    for location, script_config, script_name in ordered_locations(locations):
+        view = TreeView(root_item,
+            exclude=[l for l, n, c in locations if not l == location])
+        item = view.locate(location)
+        script = query_script(script_name)
+        if script is None:
+            raise ValueError("No script registered for name '%s'" % script_name)
+        builded.append((location, script(site, script_config, item)))
+
+    root_element = builded.pop()[1]
+    for location, element in reversed(builded):
+        root_element.setlocation(location, element)
+
+    return root_element
+
+
+def layout(element, directory):
+    if isinstance(element, ContentElement):
+        return layout_content(directory, element)
+    elif isinstance(element, BinaryElement):
+        return layout_binary(directory, element)
+    elif isinstance(element, Element):
+        return layout_container(directory, element)
+
+
+def layout_container(directory, element):
+    new_directory = join(directory, element.name)
+    mkdir(new_directory)
+    for child in element.values():
+        layout(new_directory, child)
+    return new_directory
+
+
+def layout_content(directory, element):
+    new_directory = layout_container(directory, element)
+    open(join(new_directory, "index.html"), "w").write(element.render())
+    return new_directory
+
+
+def layout_binary(directory, element):
+    copy(element.filename, directory)
+    return directory
---
+++ statics/tests/test_build.py
@@ -0,0 +1,50 @@
+""" Tests for :module:`statics.build`."""
+
+import unittest
+
+from statics.tests.util import TestWithLocalRegistry
+
+__all__ = ["TestBuild"]
+
+
+class TestBuild(unittest.TestCase, TestWithLocalRegistry):
+
+    def createItem(self):
+        from statics.item import Item
+        return Item("root", [
+            Item("a"),
+            Item("b", [
+                Item("ba"),
+                Item("bb")]),
+            Item("c")])
+
+    def test_it(self):
+        def dummy_script1(site, config, item):
+            def fun(item):
+                from statics.element import Element
+                return Element(item.name)
+            from statics.tree import externalmap
+            return externalmap(fun, item)
+
+        def dummy_script2(site, config, item):
+            element = dummy_script1(site, config, item)
+            from statics.element import Element
+            element["added"] = Element("added")
+            return element
+
+        from statics.configuration import add_script
+        add_script("script1", dummy_script1)
+        add_script("script2", dummy_script2)
+        site = None
+        locations = [("/", {}, "script1"), ("/b", {}, "script2")]
+        root_item = self.createItem()
+        from statics.build import build
+        root_element = build(site, root_item, locations=locations)
+        self.assertTrue("a" in root_element)
+        self.assertTrue("b" in root_element)
+        self.assertTrue("c" in root_element)
+        self.assertTrue(not "added" in root_element)
+        b_element = root_element["b"]
+        self.assertTrue("ba" in b_element)
+        self.assertTrue("bb" in b_element)
+        self.assertTrue("added" in b_element)

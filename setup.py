8a2b33f430eb304d00748d42c3dde3dfa7f67075
Andrey Popp <8mayday@gmail.com>
2010-07-11 01:13:09 +0400

    Package as setuptools.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+version = "0.1"
+requires = ["ordereddict", "jinja2", "configobj", "zope.component"]
+long_description = open("README", "r").read() + "\n\n" \
+                + open("CHANGES", "r").read()
+
+setup(name="statics",
+      version=version,
+      description="Static site generator.",
+      long_description=long_description,
+      keywords="web publisher staticsite",
+      author="Andrey Popp",
+      author_email="8mayday@gmail.com",
+      license="MIT",
+      packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=requires,
+      test_suite="statics.tests",
+      entry_points={"console_scripts": "statics=statics.cli:statics"},
+      )

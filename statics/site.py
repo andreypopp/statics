a1d51b6c9cf9791d41f9b5136f75fad647d042a4
Andrey Popp <8mayday@gmail.com>
2010-07-11 19:25:20 +0400

    Removed useless factory method for Site.

--- statics/site.py
+++ statics/site.py
@@ -1,31 +1,14 @@
 """ Site."""
 
-from os.path import abspath
-from os.path import dirname
-from os.path import exists
-
 from configobj import ConfigObj
 
 from statics.util import cached_generator_property
-from statics.util import cached_property
 
 __all__ = ["Site"]
 
 
 class Site(ConfigObj):
     """ Rich wrapper around :class:`configobj.ConfigObj`."""
-
-    @classmethod
-    def from_file(cls, filename):
-        """ Initialize site object from configuration file."""
-        filename = abspath(filename)
-        if not exists(filename):
-            raise ValueError("Configuration file '%s' does not exists.")
-        here = dirname(filename)
-        site = cls(filename)
-        site["DEFAULT"] = {}
-        site["DEFAULT"]["HERE"] = here
-        return site
 
     @cached_generator_property
     def locations(self):
